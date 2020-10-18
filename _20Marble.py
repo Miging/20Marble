c663760e4f5b7b0eb5461ab8c0e3c8f7b92494fc
dnfldpden <dnfldpden32@gmail.com>
2020-10-18 18:02:30 +0900

    Modified the files[Mod]

--- _20Marble.py
+++ _20Marble.py
@@ -1,14 +1,31 @@
 from bangtal import *
 import random
 
-startscene=Scene("","")
-mainscene=Scene("","")
+setGameOption(GameOption.INVENTORY_BUTTON,False)
+setGameOption(GameOption.MESSAGE_BOX_BUTTON,False)
 
-startbtn=Object()
-explainbtn=Object()
 
-explain=Object()
+startscene=Scene("시작화면","image/startimage.png")
+mainscene=Scene("게임화면","image/mainimage.png")
+
+startbtn=Object("image/startbtn.png")
+startbtn.locate(startscene,490,100)
+startbtn.show()
+
+explainbtn=Object("image/")
+
+explain=Object("image/")
 explain.hide()
+
+player1=Object("image/player1.png")
+player1.locate(mainscene,0,50)
+player1.setScale(0.5)
+player1.show()
+
+player2=Object("image/player2.png")
+player2.locate(mainscene,100,50)
+player2.setScale(0.5)
+player2.show()
 
 def on_startbtn(x,y,action):
     mainscene.enter()
@@ -16,17 +33,7 @@ startbtn.onMouseAction=on_startbtn
 
 def on_explainbtn(x,y,action):
     explain.show()
-startbtn.onMouseAction=on_explainbtn
-
-startGame(startscene)
-
-#주사위 구현
-
-dicebtn=Object()
-move=0
-def on_dicebtn(x,y,action):
-    move=random.randrange(1,7)
-dicebtn.onMouseAction=on_dicebtn
+explainbtn.onMouseAction=on_explainbtn
 
 #맵 구현
 #플레이어 1 위치 1로표기
@@ -36,7 +43,24 @@ gamemap=[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 #1이 왔다갔으면 1
 visitmap=[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-#움직임 구현
+#주사위 구현
+dicebtn=Object("")
+move=0
+def on_dicebtn(x,y,action):
+    move=random.randrange(1,7)
+    showMessage(str(move)+"이 나왔다!")
+    gamemap[gamemap.index(1)]=0
+    if gamemap.index(1)+move>32:
+        gamemap[gamemap.index(1)+move-32]=1
+        visitmap[gamemap.index(1)+move-32]=1
+    else:
+        gamemap[gamemap.index(1)+move]=1
+        visitmap[gamemap.index(1)+move]=1   
+dicebtn.onMouseAction=on_dicebtn
+
+startGame(startscene)
+
+
 
 
 
